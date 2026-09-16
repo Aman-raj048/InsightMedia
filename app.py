627d54b3cf1c3ea0ac36d3c737f5a2479d4d2c3b
Aman Raj <rajaman8217@gmail.com>
2026-09-16 23:24:41 +0530

    Adjust sidebar footer spacing

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ st.markdown("""
 /* Sidebar Footer */
 /* Sidebar Footer */
 .sidebar-footer {
-    margin-top: 98px;
+    margin-top: 10px;
     padding: 15px 5px 5px 5px;
     color: #9ca0aa;
     font-size: 14px;
